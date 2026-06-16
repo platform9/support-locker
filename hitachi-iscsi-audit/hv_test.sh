a8ae89136dd5f2a0a93cc8e81d961bb02a6fad2a
Sanya Varghese <sanya@platform.com>
2026-06-16 14:06:12 -0400

    hv_test.sh: require HITACHI_PASS env var, add LDEV 105 and per-hg lun queries

--- hitachi-iscsi-audit/hv_test.sh
+++ hitachi-iscsi-audit/hv_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 H="https://192.168.176.64/ConfigurationManager/v1/objects/storages/938000745751"
 U="openstack"
-P="${HITACHI_PASS:-openstack2025!}"
+P="${HITACHI_PASS:?HITACHI_PASS env var must be set}"
 
 hv() { curl -sk -u "$U:$P" -H "Accept: application/json" -H "Content-Type: application/json" "$H/$1"; }
 
@@ -13,9 +13,13 @@ echo "=== LDEV 129 ==="
 hv "ldevs/129"
 echo ""
 
-echo "=== LUNS CL1-D ==="
-hv "luns?portId=CL1-D&count=20"
+echo "=== LDEV 105 ==="
+hv "ldevs/105"
 echo ""
 
-echo "=== LUNS CL2-D ==="
-hv "luns?portId=CL2-D&count=20"
+echo "=== LUNS CL1-D HG6 (pn01) ==="
+hv "luns?portId=CL1-D&hostGroupNumber=6&count=20"
+echo ""
+
+echo "=== LUNS CL2-D HG5 (pn01) ==="
+hv "luns?portId=CL2-D&hostGroupNumber=5&count=20"
